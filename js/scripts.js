6379af228c01c7524a59d10d40f073da2b0f18a7
Kevin Munene <lukekevin39@gmail.com>
2020-02-23 19:18:24 +0300

    add final changes

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    let designpara = $('designpara');
+    let developpara = $('developpara');
+    let managepara = $('manaepara');
+
+$(designpara) .hide();
+$(developpara) .hide();
+$(managepara) .hide();
+});
