616d64569c7c7a41ba349f4616d698f782b3df8f
Kevin Munene <lukekevin39@gmail.com>
2020-02-24 12:26:40 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,30 @@ $(document).ready(function() {
         $(".management").toggle(100);
         $(".click3").toggle(100);
     });
+    $(".first").hover(function(){
+        $("#1").toggle()
+    })
+    $(".second").hover(function(){
+        $("#2").toggle()
+    })
+    $(".third").hover(function(){
+        $("#3").toggle()
+    })
+    $(".fourth").hover(function(){
+        $("#4").toggle()
+    })
+    $(".fifth").hover(function(){
+        $("#5").toggle()
+    })
+    $(".sixth").hover(function(){
+        $("#6").toggle()
+    })
+    $(".seventh").hover(function(){
+        $("#7").toggle()
+    })
+    $(".eighth").hover(function(){
+        $("#8").toggle()
+    })
 })
 
 function validation() {
@@ -29,3 +53,6 @@ function validation() {
         alert("cheers " + name + " ,we will get back to you");
     }
 }
+
+
+
